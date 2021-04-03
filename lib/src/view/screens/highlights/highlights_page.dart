1b6bf4b1d6b6214ddc91e6be3cb98cf07725b10e
Leo Elstin <leoelstin@gmail.com>
2021-04-03 14:39:45 +0530

    added Highlights added Flutter chart plugin

---
+++ lib/src/view/screens/highlights/highlights_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HighlightsPage extends StatefulWidget {
+  @override
+  _HighlightsPageState createState() => _HighlightsPageState();
+}
+
+class _HighlightsPageState extends State<HighlightsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
